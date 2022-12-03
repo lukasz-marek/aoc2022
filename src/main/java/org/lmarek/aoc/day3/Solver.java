4d225f29983dd9ee0e232555ba529b46119cfb39
Łukasz Marek <marek.lucas@gmail.com>
2022-12-03 14:15:28 +0100

    day 3 part 2 done

--- src/main/java/org/lmarek/aoc/day3/Solver.java
+++ src/main/java/org/lmarek/aoc/day3/Solver.java
@@ -1,5 +1,7 @@
 package org.lmarek.aoc.day3;
 
+import org.apache.commons.collections4.ListUtils;
+
 class Solver {
   public static void main(String[] args) {
     var rucksacks = InputLoader.load();
@@ -10,5 +12,10 @@ class Solver {
             .flatMapToLong(items -> items.stream().mapToLong(Item::priority))
             .sum();
     System.out.println("Part 1: " + sumOfPriorities);
+
+    var groups = ListUtils.partition(rucksacks, 3).stream().map(Group::new).toList();
+    var sumOfBadgePriorities =
+        groups.stream().map(Group::findBadge).mapToLong(Item::priority).sum();
+    System.out.println("Part 2: " + sumOfBadgePriorities);
   }
 }
